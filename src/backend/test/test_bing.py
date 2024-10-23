624608f4ca808ec73a14c1686b73b506014ad970
lixiang <lixiang.cosmoplat@haier.com>
2024-10-23 10:26:06 +0800

    feat: bing search api test

--- src/backend/test/test_bing.py
+++ src/backend/test/test_bing.py
@@ -5,9 +5,20 @@ BING_SEARCH_URL = get_config("api.bing.endpoint")
 BING_SUBSCRIPTION_KEY = get_config("api.bing.api_key")
 search = BingSearchAPIWrapper(bing_subscription_key=BING_SUBSCRIPTION_KEY,
                               bing_search_url=f"{BING_SEARCH_URL}/v7.0/search",
-                              k=4)
+                              k=5)
 
-result = search.run("西安的天气怎么样？")
-origin = search.results("西安的天气怎么样？")
-print(result) # 结果信息
-print(origin) # 溯源信息
+results = search.results("西安的天气怎么样？", 5)
+snippets = [] # 结果集合
+origins = [] # 溯源集合
+if len(results) == 0:
+    print("No good Bing Search Result was found")
+for result in results:
+    snippets.append(result["snippet"])
+    origin_dict = {
+        "title": str(result["title"]).replace("<b>","").replace("</b>",""),
+        "link": result["link"],
+    }
+    origins.append(origin_dict)
+
+print(snippets) # 检索信息
+print(origins) # 溯源信息
