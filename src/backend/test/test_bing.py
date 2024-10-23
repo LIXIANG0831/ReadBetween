1d3afe4e44153338784eae3ac30081282caf1f54
lixiang <lixiang.cosmoplat@haier.com>
2024-10-23 09:47:38 +0800

    feat: bing search api test

---
+++ src/backend/test/test_bing.py
@@ -0,0 +1,11 @@
+from langchain_community.utilities import BingSearchAPIWrapper
+from awsome.settings import get_config
+
+BING_SEARCH_URL = get_config("api.bing.endpoint")
+BING_SUBSCRIPTION_KEY = get_config("api.bing.api_key")
+search = BingSearchAPIWrapper(bing_subscription_key=BING_SUBSCRIPTION_KEY,
+                              bing_search_url=f"{BING_SEARCH_URL}/v7.0/search",
+                              k=4)
+
+result = search.run("西安的天气怎么样？")
+print(result)
