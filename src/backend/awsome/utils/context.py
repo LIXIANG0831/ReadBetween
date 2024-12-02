6f83a3f171d3c6405c73d2b875d327bc9fdf54ca
lixiang <lixiang.cosmoplat@haier.com>
2024-12-02 19:53:25 +0800

    [Feat]异步文件打开

--- src/backend/awsome/utils/context.py
+++ src/backend/awsome/utils/context.py
@@ -2,6 +2,8 @@ from contextlib import contextmanager
 from sqlmodel import Session
 from awsome.utils.database_client import database_client
 from awsome.utils.logger_util import logger_util
+import asyncio
+import aiofiles
 
 
 @contextmanager
@@ -25,3 +27,28 @@ def file_open(file_name, mode):
         yield f
     finally:
         f.close()
+
+
+async def async_file_open(file_name, mode='rb'):
+    """
+    异步打开文件并返回文件对象。
+
+    参数:
+        file_name (str): 要打开的文件名称。
+        mode (str): 打开文件的模式，默认为 'r'。
+
+    返回:
+        aiofiles.File: 打开的文件对象。
+    """
+    try:
+        # 记录日志，尝试打开文件
+        logger_util.info(f"尝试以模式 {mode} 打开文件 {file_name}")
+        async with aiofiles.open(file_name, mode) as file:
+            yield file
+    except Exception as e:
+        # 记录日志，打开文件失败
+        logger_util.error(f"打开文件 {file_name} 失败：{e}")
+        raise
+    finally:
+        # 记录日志，文件已关闭
+        logger_util.info(f"文件 {file_name} 已关闭")
