4a9b53efcbe5219ea6ead3195c5dd2341c673e67
lixiang <lixiang.cosmoplat@haier.com>
2024-12-15 02:00:24 +0800

    [Test]LiveKit

---
+++ src/backend/test/test_livekit/livekit_agent/server.py
@@ -0,0 +1,23 @@
+# server.py
+import os
+from livekit import api
+from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+
+@app.get("/getToken")
+async def get_token():
+    token = api.AccessToken(os.getenv('LIVEKIT_API_KEY'), os.getenv('LIVEKIT_API_SECRET')) \
+        .with_identity("identity") \
+        .with_name("my name") \
+        .with_grants(api.VideoGrants(
+        room_join=True,
+        room="my-room",
+    ))
+    return {"token": token.to_jwt()}
+
+# uvicorn server:app --reload
